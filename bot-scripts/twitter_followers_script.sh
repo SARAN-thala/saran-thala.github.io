869743ce554207b15c49b4806bd1631963a5d427
Saran <saranrajmalar@gmail.com>
2017-08-10 21:15:40 +0530

    [MY-BOT] Updating twitter followers count

--- bot-scripts/twitter_followers_script.sh
+++ bot-scripts/twitter_followers_script.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -ex
-fetch_twitter_followers.py
+python ./fetch_twitter_followers.py
 
 GIT_DIFF_FILES=$(git diff --name-only)
 
 if [[ "$GIT_DIFF_FILES" =~ "bot-scripts/twitter_followers_count.json" ]]; then
-    git add bot-scripts/twitter_follower_count.json
-    git commit -m "[MY-BOT] Updating twitter followers count"
+#    git add ./twitter_follower_count.json
+    git commit -am "[MY-BOT] Updating twitter followers count"
     git push origin master
 else
     echo "Follower count unchanged"
